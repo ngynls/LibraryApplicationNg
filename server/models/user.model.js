39e32cfabcd8e08a18ad9a26a3176fef200d10b7
ngynls <ngyn.ls@gmail.com>
2019-06-26 14:57:52 -0400

    User schema

---
+++ server/models/user.model.js
@@ -0,0 +1,38 @@
+const mongoose=require('mongoose');
+const bcrypt=require('bcryptjs');
+const jwt=require('jsonwebtoken');
+const jwt_secret=require('../config/keys').JWT_SECRET;
+
+const userSchema=new mongoose.Schema({
+    username:{
+        type:String,
+        required: 'Username cannot be empty',
+        unique: true
+    },
+    password:{
+        type:String,
+        required: 'Password cannot be empty',
+        minlength:[6, 'Password must be at least 6 characters long']
+    },
+    saltSecret: String
+});
+
+userSchema.pre('save', function(next){
+    bcrypt.genSalt(10,(err,salt)=>{
+        bcrypt.hash(this.password, salt, (err,hash)=>{
+            this.password=hash;
+            this.saltSecret=salt;
+            next();
+        });
+    });
+});
+
+userSchema.methods.verifyPassword= function(password){
+    return bcrypt.compareSync(password, this.password);
+};
+
+userSchema.methods.generateJwt= function(){
+    return jwt.sign({_id: this._id}, jwt_secret, {expiresIn: "2m"});
+}
+
+module.exports=User=mongoose.model('User', userSchema);
