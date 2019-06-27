6ea36ea296aed2c749bdcb0b464a41a3a9806ab2
ngynls <ngyn.ls@gmail.com>
2019-06-26 22:33:25 -0400

    CRUD functionalities in publisher api

---
+++ server/api/publishers.js
@@ -0,0 +1,50 @@
+const express=require('express');
+const router=express.Router();
+const Publisher=require('../models/publisher.model');
+
+// GET : All publishers
+router.get('/', (req,res)=>{
+    Publisher.find().then(publishers=>{
+        res.status(200).json(publishers);
+    }).catch(err=>{
+        res.status(500).json({message: err.message});
+    });
+});
+
+// GET: A particular publisher
+router.get('/:id', (req,res)=>{
+    Publisher.findById(req.params.id)
+    .then(publisher => {
+        if (!publisher)
+            res.status(404).send('Publisher was not found');
+        else
+            res.status(200).json(publisher);
+    });
+});
+
+// POST: A new publisher
+router.post('/', (req,res)=>{
+    const newPublisher=new Publisher({
+        publisherName: req.body.publisherName,
+        location: req.body.location
+    });
+    newPublisher.save()
+    .then(publisher=> res.json(publisher))
+    .catch(err => res.json({error: err.message}));
+});
+
+// PUT: Update a publisher
+router.put('/:id', (req,res)=>{
+    Publisher.findByIdAndUpdate(req.params.id, req.body)
+    .catch(err=> res.status(404).json({error: err.message}));
+    res.send('Publisher was successfully updated!');
+});
+
+// DELETE: A particular publisher
+router.delete('/:id', (req,res)=>{
+    Publisher.findByIdAndDelete(req.params.id)
+    .catch(err=> res.status(404).json({error:err.message}));
+    res.status(200).json({message: 'Publisher is successfully deleted from db'});
+});
+
+module.exports=router;
--- server/server.js
+++ server/server.js
@@ -10,12 +10,14 @@ app.use(passport.initialize());
 
 const db=require('./config/keys').mongoURL;
 const authors=require('./api/authors');
+const publishers=require('./api/publishers');
 
 mongoose.connect(db)
   .then(()=> console.log('Connected!'))
   .catch(err => console.log(err));
 
 app.use('/authors', authors);
+app.use('/publishers', publishers);
 
 const port= process.env.PORT || 5000;
 
