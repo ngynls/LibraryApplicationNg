87279b562e191c5de58c0664b79b77966a6cc5b9
ngynls <ngyn.ls@gmail.com>
2019-07-09 21:52:10 -0400

    Genre service

---
+++ client/src/app/shared/services/genre.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GenreService {
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type':  'application/json'
+    })
+  };
+
+  constructor(private http: HttpClient) { }
+
+  getGenres(){
+    return this.http.get(environment.apiUrl + '/genres');
+  }
+
+  getGenre(id:string){
+    return this.http.get(environment.apiUrl + '/genres/' + id);
+  }
+
+  addGenre(genre){
+    return this.http.post(environment.apiUrl + '/genres', genre, this.httpOptions);
+  }
+
+  updateGenre(id:string, genre){
+    return this.http.put(environment.apiUrl + '/genres/'+ id, genre , this.httpOptions);
+  }
+
+  deleteGenre(id:string){
+    return this.http.delete(environment.apiUrl + '/genres/' + id);
+  }
+
+}
