1b76cbe4c010af34357b1b438de3d53e7c1f747c
ngynls <ngyn.ls@gmail.com>
2019-07-11 17:24:23 -0400

    Library member service

---
+++ client/src/app/shared/services/member.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MemberService {
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type':  'application/json'
+    })
+  };
+
+  constructor(private http:HttpClient) { }
+
+  getMembers(){
+    return this.http.get(environment.apiUrl + '/members');
+  }
+
+  getMember(id:string){
+    return this.http.get(environment.apiUrl + '/members/' + id);
+  }
+
+  addMember(member){
+    return this.http.post(environment.apiUrl + '/members', member, this.httpOptions);
+  }
+
+  updateMember(id:string, member){
+    return this.http.put(environment.apiUrl + '/members/'+ id, member , this.httpOptions);
+  }
+
+  deleteMember(id:string){
+    return this.http.delete(environment.apiUrl + '/members/' + id);
+  }
+
+}
