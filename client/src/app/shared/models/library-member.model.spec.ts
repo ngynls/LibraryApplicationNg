29991b5e9c28671937eac86ca246ba9fe7540755
ngynls <ngyn.ls@gmail.com>
2019-07-11 17:06:50 -0400

    Library member model

---
+++ client/src/app/shared/models/library-member.model.spec.ts
@@ -0,0 +1,7 @@
+import { LibraryMember } from './library-member.model';
+
+describe('LibraryMember', () => {
+  it('should create an instance', () => {
+    expect(new LibraryMember()).toBeTruthy();
+  });
+});
---
+++ client/src/app/shared/models/library-member.model.ts
@@ -0,0 +1,12 @@
+export class LibraryMember {
+  _id?: string;
+  firstName: string;
+  lastName: string;
+  dob: string;
+  address: string;
+  postalCode: string;
+  telephone: string;
+  email: string;
+  loans: string[];
+  reservations: string[];
+}
