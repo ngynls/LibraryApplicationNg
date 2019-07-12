e6788958138a2e0f83b35921c88235de6b72daea
ngynls <ngyn.ls@gmail.com>
2019-07-12 17:54:25 -0400

    Book copy service

---
+++ client/src/app/shared/services/book-copy.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BookCopyService {
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type':  'application/json'
+    })
+  };
+
+  constructor(private http:HttpClient) { }
+
+  getBookCopies(){
+    return this.http.get(environment.apiUrl + '/bookCopies');
+  }
+
+  getBookCopy(id:string){
+    return this.http.get(environment.apiUrl + '/bookCopies/' + id);
+  }
+
+  addBookCopy(copy){
+    return this.http.post(environment.apiUrl + '/bookCopies', copy, this.httpOptions);
+  }
+
+  editBookCopy(id:string, copy){
+    return this.http.put(environment.apiUrl + '/bookCopies/' + id, copy, this.httpOptions);
+  }
+
+  deleteBookCopy(id:string){
+    return this.http.delete(environment.apiUrl + '/bookCopies/' + id);
+  }
+}
