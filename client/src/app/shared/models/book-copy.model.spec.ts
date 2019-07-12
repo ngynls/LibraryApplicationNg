5ac2994a95fd62a8bcde0aa01471bc3ce1eb910d
ngynls <ngyn.ls@gmail.com>
2019-07-12 17:41:21 -0400

    Book copy model for client

---
+++ client/src/app/shared/models/book-copy.model.spec.ts
@@ -0,0 +1,7 @@
+import { BookCopy } from './book-copy.model';
+
+describe('BookCopy', () => {
+  it('should create an instance', () => {
+    expect(new BookCopy()).toBeTruthy();
+  });
+});
---
+++ client/src/app/shared/models/book-copy.model.ts
@@ -0,0 +1,6 @@
+export class BookCopy {
+  _id?: string;
+  copyName: string;
+  bookId: string;
+  status: string;
+}
