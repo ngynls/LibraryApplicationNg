f3eacf38d8a3f81380fb257805a1211f7e730a52
ngynls <ngyn.ls@gmail.com>
2019-07-15 12:13:21 -0400

    Reservation service

---
+++ client/src/app/shared/services/reservation.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ReservationService {
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type':  'application/json'
+    })
+  };
+
+  constructor(private http:HttpClient) { }
+
+  getReservations(){
+    return this.http.get(environment.apiUrl + '/reservations');
+  }
+
+  getReservation(id:string){
+    return this.http.get(environment.apiUrl + '/reservations/' + id);
+  }
+
+  addReservation(reservation){
+    return this.http.post(environment.apiUrl + '/reservations', reservation, this.httpOptions);
+  }
+
+  updateReservation(id:string, reservation){
+    return this.http.put(environment.apiUrl + '/reservations/'+ id, reservation , this.httpOptions);
+  }
+
+  deleteReservation(id:string){
+    return this.http.delete(environment.apiUrl + '/reservations/' + id);
+  }
+
+}
