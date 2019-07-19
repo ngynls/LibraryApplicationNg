09b4fe692de856723f6e8bfa122228961cae65f0
ngynls <ngyn.ls@gmail.com>
2019-07-19 15:05:46 -0400

    Book copies by status chart

--- client/src/app/pages/dashboard/dashboard.component.ts
+++ client/src/app/pages/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { BookCopyService } from 'src/app/shared/services/book-copy.service';
+import { BookCopy } from 'src/app/shared/models/book-copy.model';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  private copies:BookCopy[];
+  private availableCopies:number;
+  private loanedCopies: number;
+  private reservedCopies: number;
+  public pieChartLabels = ['Available', 'Loaned', 'Reserved'];
+  public pieChartData= [];
+  public pieChartType = 'pie';
+  public pieChartColors = [
+    {
+      backgroundColor: ['rgba(255,0,0,0.3)', 'rgba(0,255,0,0.3)', 'rgba(0,0,255,0.3)'],
+    },
+  ];
+
+
+  constructor(private copyService:BookCopyService) { }
 
   ngOnInit() {
+    this.copyService.getBookCopies().subscribe((data:BookCopy[])=>{
+      this.availableCopies=data.filter(copy => copy.status === 'Available').length;
+      this.pieChartData.push(this.availableCopies);
+      this.loanedCopies=data.filter(copy => copy.status === 'On loan').length;
+      this.pieChartData.push(this.loanedCopies);
+      this.reservedCopies=data.filter(copy => copy.status === 'Reserved').length;
+      this.pieChartData.push(this.reservedCopies);
+    })
   }
 
 }
